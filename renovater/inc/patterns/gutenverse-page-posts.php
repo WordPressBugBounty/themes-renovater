0723218d75d3d13e0825acf2c645c3d945ec570b
Sean Murphy <sean@iamseanmurphy.com>
2026-05-06 05:20:37 +0000

    1.1.0

---
+++ renovater/inc/patterns/gutenverse-page-posts.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Gutenverse content.
+ */
+return array(
+	'title'      => __( 'Gutenverse Page Post', 'renovater' ),
+	'categories' => array( 'renovater-gutenverse' ),
+	'content'    => '<!-- wp:gutenverse/post-content {"elementId":"guten-177AgY"} -->
+<div class="guten-element guten-post-content guten-177AgY"></div>
+<!-- /wp:gutenverse/post-content -->',
+);
